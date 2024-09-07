94e0f444fdde637e228a1dd8c1c80382384c5abe
igor komendantov <komendantov2580@gmail.com>
2024-09-07 16:56:31 +0300

    create MarkIcon component

---
+++ src/entities/ttt/ui/MarkIcon.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Mark } from '../types';
+import CrossIcon from '@/shared/assets/cross.svg'
+import ZeroIcon from '@/shared/assets/zero.svg'
+import { SVGAttributes } from 'vue';
+
+interface Props {
+  mark: Mark
+}
+const { mark } = defineProps<Props & SVGAttributes>()
+
+const icons: Record<Mark, string> = {
+  'cross': CrossIcon,
+  'zero': ZeroIcon
+}
+
+</script>
+
+<template>
+  <component :is="icons[mark]" />
+</template>
