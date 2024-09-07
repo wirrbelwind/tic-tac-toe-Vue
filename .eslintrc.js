703acea4867d56d0f9b86c3a3f70a4b3dd8b956d
igor komendantov <komendantov2580@gmail.com>
2024-09-07 13:12:30 +0300

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/**
+ * .eslint.js
+ *
+ * ESLint configuration file.
+ */
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    '@vue/eslint-config-typescript',
+    './.eslintrc-auto-import.json',
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+}
