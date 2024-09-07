7292f24eaf405208f7fffaea4b7003b444fa7761
igor komendantov <komendantov2580@gmail.com>
2024-09-07 13:35:15 +0300

    render game-stage components

---
+++ src/pages/TicTacToe.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import GameSettings from '@/components/GameSettings.vue'
+  import GameBoard from '@/components/GameBoard.vue'
+  import GameResults from '@/components/GameResults.vue'
+  import { useTicTacToe } from '@/entities/ttt/useTicTacToe'
+  import { GameStatus } from '@/entities/ttt/types'
+
+  const ttt = useTicTacToe()
+
+  const gameSteps: Record<GameStatus, any> = {
+    idle: GameSettings,
+    inProgress: GameBoard,
+    finished: GameResults,
+  }
+
+</script>
+
+<template>
+  <div class="cursor d-flex align-center justify-center h-screen">
+    <component :is="gameSteps[ttt.status]" />
+  </div>
+</template>
