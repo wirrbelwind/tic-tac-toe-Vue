6458fccffc2102ec642992114a3cd8a63651c758
igor komendantov <komendantov2580@gmail.com>
2024-09-07 13:31:32 +0300

    create types for ttt

---
+++ src/entities/ttt/types.ts
@@ -0,0 +1,19 @@
+type Mark = 'cross' | 'zero'
+
+export interface Player {
+  name: string
+  mark: Mark
+}
+
+export type GameStatus = 'idle' | 'inProgress' | 'finished'
+
+export interface GameState {
+  status: GameStatus
+  settings: GameSettings | null
+}
+
+export interface GameSettings {
+  boardSize: number
+  player1: Player
+  player2: Player
+}
