4179f5f333bc22b6423d752ddf527f9cec659d28
igor komendantov <komendantov2580@gmail.com>
2024-09-07 13:32:05 +0300

    create pinia state for ttt

---
+++ src/entities/ttt/useTicTacToe.ts
@@ -0,0 +1,23 @@
+// stores/counter.js
+import { defineStore } from 'pinia'
+import { GameSettings, GameState } from './types'
+
+export const useTicTacToe = defineStore(
+  'tic-tac-toe',
+  {
+    state: (): GameState => ({
+      status: 'idle',
+      settings: null
+    }),
+    actions: {
+
+      startGame(newSettings: GameSettings) {
+        this.settings = newSettings
+        this.status = 'inProgress'
+      },
+      endGame() {
+        this.status = 'finished'
+      }
+    },
+  }
+)
